cae7ffad2308d45733645ee2a2ba412532052454
Kevin0217168 <15838189794@163.com>
2020-04-07 00:19:59 +0800

    Release V1.0 多词条下载内容及图片 支持指定下载目录 暂不支持百度文库

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ sys.path.append(r"C:\Users\18037802106\AppData\Local\Programs\Python\Python37\Li
 
 import title
 
+
+import downLoad
+
 import os
+
+dira = input("请输入程序的工作目录(不填则为程序所在目录)：")
+if dira == "":
+    dira = os.getcwd()
+dow = downLoad.Download(dira)
 
 ba = input("请输入要搜索的吧名(不填则为全吧搜索)：")
 word = input("请输入要搜索的关键词(必填)：")
@@ -28,7 +36,7 @@ while True:
         
     print("您所在的位置是第", page, "页，共", Req.tail_page, "页，请选择需要的指令：")
     print("向上翻页：j，向下翻页：k，跳转到页面：goto {page}，退出：q")
-    print("输入文章序号来下载内容(可多个下载)：get {paperNum} [paper2Num] [paper3Num]...")
+    print("输入文章序号来下载内容(可多个下载)：get {paperNum}|[paper2Num]|[paper3Num]|...")
     
     while True:
         # 请求输入并去除左右空格
@@ -61,12 +69,15 @@ while True:
         
         # 下载页面
         elif "get" in ch:
-            num_list = ch[4:-1].split(" ")
+            num_list = ch[4:-1].split("|")
             print("注意！将要下载这些页面：", num_list)
             if input("按'y'确认：") != 'y':
                 print("您取消了下载！")
                 continue
             print("准备下载...")
+            home_url = "https://tieba.baidu.com"
+            for i in num_list:
+                dow.dwonLoad(home_url + res[1][res[0][int(i) - 1]])
             
         # 退出
         elif ch == "q":
